14d9408ec94a29a9d2daa8a2248d6f2988d000fb
SanuviGamage5043 <sanuvigamage20842@gmail.com>
2025-01-14 14:30:16 +0530

    menubar layout modify

--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -4,7 +4,7 @@ import "./MenuBar.css";
 export default function MenuBar() {
   return (
     <div className="listtgroup">
-    <ul >
+    <ul className="list">
         <li className="listcomp">Occasions</li>
         <li className="listcomp">Flowers</li>
         <li className="listcomp">Gift Baksets & Boxes</li>
--- src/pages/Home2/Home2.jsx
+++ src/pages/Home2/Home2.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "@/components/Header/Header";
 import Gallery from "@/components/Gallery/Gallery";
+import MenuBar from '@/components/MenuBar/MenuBar'
 export default function Home2() {
   return (
     <div>
       <Header/>
+      <MenuBar/>
+      
        {/* <Gallery/>  */}
     </div>
   );
