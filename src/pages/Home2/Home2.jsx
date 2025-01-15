5eb7bc623a628cdeabd56c0d629114265b0f99ea
SanuviGamage5043 <sanuvigamage20842@gmail.com>
2025-01-15 22:43:10 +0530

    home page styling

--- src/pages/Home2/Home2.jsx
+++ src/pages/Home2/Home2.jsx
@@ -7,9 +7,41 @@ import MenuBar from "../../components/MenuBar/MenuBar";
 
 export default function Home2( cart ) {
   return (
-    <div>
+    <div className="main-container">
       <Header cart={cart}/>
       <MenuBar/>
+      <br/>
+      <br/>
+      <br/>
+      <div className="decohome">
+      <div className="heading1main">
+        <div className="allheadings">
+        <h1 className="heading1">
+        Every gift is a story waiting to be told
+        </h1>
+        <br/>
+        <br/>
+       
+        <br/>
+        <h3 className="heading3"> 
+          Get a 50% Discount
+        </h3>
+        <br/>
+        <br/>
+      
+       
+        <h2 className="heading2">
+        Spread smiles, one gift at a time.
+        </h2>
+
+        </div>
+        
+        <img src="/images/wall1.png" className="imagewall"/>
+      </div>
+      
     </div>
+
+      </div>
+      
   );
 }
