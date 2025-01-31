7b606da2e9667b108c4cc8b04d5a7cbcc4a60fbb
SanuviGamage5043 <sanuvigamage20842@gmail.com>
2025-01-31 18:34:44 +0530

    chcolate backend creating

--- src/pages/Choco/Choco.jsx
+++ src/pages/Choco/Choco.jsx
@@ -1,29 +1,47 @@
-import React from "react";
+import React, { useEffect, useState }  from "react";
 import Header from "../../components/Header/Header";
 import MenuBar from "../../components/MenuBar/MenuBar";
 import ProductCard from "../../components/ProductCard/ProductCard";
 import "./Choco.css"; 
+import axios from "axios";
 
-const chocoProducts = [
-  {
-    id: 1,
-    image:
-      "https://cdn.builder.io/api/v1/image/assets/TEMP/492319edd1ea21b96461268a3321fed583c751bbc6551c7fb3c59b1437011ff3?placeholderIfAbsent=true&apiKey=dab43acfe9e64948b6d46280c551ed17",
-    title: "KitKat Bar",
-    price: 2500.0,
-    quantity: 1,
-  },
+// const chocoProducts = [
+//   {
+//     id: 1,
+//     image:
+//       "https://cdn.builder.io/api/v1/image/assets/TEMP/492319edd1ea21b96461268a3321fed583c751bbc6551c7fb3c59b1437011ff3?placeholderIfAbsent=true&apiKey=dab43acfe9e64948b6d46280c551ed17",
+//     title: "KitKat Bar",
+//     price: 2500.0,
+//     quantity: 1,
+//   },
 
-  {
-    id:2,
-    image: "https://cdn.builder.io/api/v1/image/assets/TEMP/84528618be7628cfaab6af15635b61b7ed1a0d45ad5a065fe88313aa0cb68fca?placeholderIfAbsent=true&apiKey=dab43acfe9e64948b6d46280c551ed17",
-    title: "Ferrero Rocher",
-    price: 10000.00,
-    quantity:1,
-  },
-];
+//   {
+//     id:2,
+//     image: "https://cdn.builder.io/api/v1/image/assets/TEMP/84528618be7628cfaab6af15635b61b7ed1a0d45ad5a065fe88313aa0cb68fca?placeholderIfAbsent=true&apiKey=dab43acfe9e64948b6d46280c551ed17",
+//     title: "Ferrero Rocher",
+//     price: 10000.00,
+//     quantity:1,
+//   },
+// ];
 
 export default function Choco({addToCart, cart}) {
+  const [products, setProducts] = useState([])
+
+   // Fetch all products
+   const fetchProducts = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/products/Chocolate");
+      setProducts(response.data);
+      console.log(products)
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <main className="main-container">
       <Header cart={cart} />
@@ -31,7 +49,7 @@ export default function Choco({addToCart, cart}) {
       <section className="products-section">
         <h2 className="section-title">Chocolate</h2>
         <div className="products-grid">
-          {chocoProducts.map((product) => (
+          {products.map((product) => (
             <div key={product.id} className="product-item">
               <ProductCard 
                   product={product} 
--- src/pages/GiftBox/GiftBox.jsx
+++ src/pages/GiftBox/GiftBox.jsx
@@ -1,37 +1,55 @@
-import React from "react";
+import React, { useEffect, useState }  from "react";
 import Header from "../../components/Header/Header";
 import MenuBar from "../../components/MenuBar/MenuBar";
 import ProductCard from "../../components/ProductCard/ProductCard";
 import "./GiftBox.css";
+import axios from "axios";
 
-const giftboxProducts = [
-  {
-    id: 1,
-    image:
-      "https://cdn.builder.io/api/v1/image/assets/TEMP/dbb0a248c0b2073baf4d52cf5ea94ae44a3afd46a6db09ef44069a0b6b354c62?placeholderIfAbsent=true&apiKey=dab43acfe9e64948b6d46280c551ed17",
-    title: "Fresh Fruits Basket",
-    price: 10000.0,
-    quantity: 1,
-  },
-  {
-    id: 2,
-    image:
-      "https://cdn.builder.io/api/v1/image/assets/TEMP/4dd9d5771291718206afbd093bc555c276cda929f3a4ad02a75083dd9cfa7ce0?placeholderIfAbsent=true&apiKey=dab43acfe9e64948b6d46280c551ed17",
-    title: "Ferrero Rocher Box",
-    price: 15000.0,
-    quantity: 1,
-  },
-];
+// const giftboxProducts = [
+//   {
+//     id: 1,
+//     image:
+//       "https://cdn.builder.io/api/v1/image/assets/TEMP/dbb0a248c0b2073baf4d52cf5ea94ae44a3afd46a6db09ef44069a0b6b354c62?placeholderIfAbsent=true&apiKey=dab43acfe9e64948b6d46280c551ed17",
+//     title: "Fresh Fruits Basket",
+//     price: 10000.0,
+//     quantity: 1,
+//   },
+//   {
+//     id: 2,
+//     image:
+//       "https://cdn.builder.io/api/v1/image/assets/TEMP/4dd9d5771291718206afbd093bc555c276cda929f3a4ad02a75083dd9cfa7ce0?placeholderIfAbsent=true&apiKey=dab43acfe9e64948b6d46280c551ed17",
+//     title: "Ferrero Rocher Box",
+//     price: 15000.0,
+//     quantity: 1,
+//   },
+// ];
 
 export default function GiftBox({ addToCart, cart }) {
+  const [products, setProducts] = useState([])
+
+   // Fetch all products
+   const fetchProducts = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/products/GiftBaskets&Boxes");
+      setProducts(response.data);
+      console.log(products)
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <main className="main-container">
       <Header cart={cart} />
       <MenuBar />
       <section className="products-section">
-        <h2 className="section-title">Gift Box</h2>
+        <h2 className="section-title">Gift Baskets & Boxes</h2>
         <div className="products-grid">
-          {giftboxProducts.map((product) => (
+          {products.map((product) => (
             <div key={product.id} className="product-item">
               <ProductCard 
               product={product} 
